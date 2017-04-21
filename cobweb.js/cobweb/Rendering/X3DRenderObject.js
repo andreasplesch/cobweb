f38d9b0dfb3668b3b87abc60b9731f92a17a1e29
Sgeo <sgeoster@gmail.com>
2017-04-21 00:06:06 -0400

    Attempt roomscale. Defect # something or other: Everything is too big. Also the stage is acting weird in Chrome. Maybe Firefox better?

--- cobweb.js/cobweb/Rendering/X3DRenderObject.js
+++ cobweb.js/cobweb/Rendering/X3DRenderObject.js
@@ -820,7 +820,16 @@ function ($,
                 this .vrDisplay .getFrameData(this .vrFrameData);
                 vrProjectionMatrices = [this .vrFrameData .leftProjectionMatrix, this .vrFrameData .rightProjectionMatrix];
                 vrViewMatrices = [this .vrFrameData .leftViewMatrix, this .vrFrameData .rightViewMatrix];
+                console.log("Left viewMatrix before staging", this .vrFrameData .leftViewMatrix);
                 EYES = 2;
+                if (this .vrDisplay .stageParameters) {
+                    var invStageMatrix = new Matrix4().assign(this .vrDisplay .stageParameters .sittingToStandingTransform) .inverse ();
+                    console.log(invStageMatrix);
+                    for(var i = 0; i < 2; i++) {
+                        vrViewMatrices[i] = new Float32Array(new Matrix4().assign(vrViewMatrices[i]).multLeft(invStageMatrix))
+                    }
+                }
+                console.log("Left viewMatrix after staging", vrViewMatrices[0]);
             }
 
 
